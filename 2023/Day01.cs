e65e614b7e09d493dd9ccc304f49e10a14c98bba
Keltusamor <d.schiffhorst95@gmx.de>
2024-03-01 16:31:31 +0100

    refactor day01

--- 2023/Day01.cs
+++ 2023/Day01.cs
@@ -7,58 +7,63 @@ public partial class Day01
     [Fact]
     public void Part1()
     {
-        Assert.Equal(52974, _Part1(File.ReadAllText("Day01Input.txt")));
-    }
-
-    private static int _Part1(string input)
-    {
-        return FindFirstAndLastNumberFromZeroToNine()
-            .Matches(input)
-            .Select(match => string.Concat(match.Groups["first"].Value, match.Groups["last"].Value))
-            .Select(int.Parse)
-            .Sum();
+        Assert.Equal(52974, Deserialize(File.ReadAllText("Day01Input.txt"), FindFirstAndLastNumberFromZeroToNine())
+            .Sum(x => x.CombinedNumber));
     }
 
     // First group has to be a lookahead because input might have only one number so it should not be consumed
-    [GeneratedRegex("(?=(?'first'[0-9])).*(?'last'[0-9])")]
+    [GeneratedRegex("(?=(?'first'[0-9])).*(?'last'[0-9])", RegexOptions.Multiline)]
     private static partial Regex FindFirstAndLastNumberFromZeroToNine();
 
     [Fact]
     public void Part2()
     {
-        Assert.Equal(53340, _Part2(File.ReadAllText("Day01Input.txt")));
+        var entries = Deserialize(File.ReadAllText("Day01Input.txt"), FindFirstAndLastNumberFromZeroToNineIncludingWords());
+        entries.ForEach(entry =>
+        {
+            entry.FirstNumber = ConvertWordToNumber(entry.FirstNumber);
+            entry.LastNumber = ConvertWordToNumber(entry.LastNumber);
+        });
+        Assert.Equal(53340, entries.Sum(x => x.CombinedNumber));
     }
 
-    private static int _Part2(string input)
-    {
-        return FindFirstAndLastNumberFromZeroToNineIncludingWords()
-            .Matches(input)
-            .Select(match => string.Concat(
-                ConvertWordToNumber(match.Groups["first"].Value),
-                ConvertWordToNumber(match.Groups["last"].Value)))
-            .Select(int.Parse)
-            .Sum();
-    }
+    // First group has to be a lookahead because input might have only one number so it should not be consumed
+    [GeneratedRegex("(?=(?'first'[0-9]|zero|one|two|three|four|five|six|seven|eight|nine)).*(?'last'[0-9]|zero|one|two|three|four|five|six|seven|eight|nine)", RegexOptions.Multiline)]
+    private static partial Regex FindFirstAndLastNumberFromZeroToNineIncludingWords();
 
     private static string ConvertWordToNumber(string number)
     {
         return number switch
         {
-            "0" or "zero" => "0",
-            "1" or "one" => "1",
-            "2" or "two" => "2",
-            "3" or "three" => "3",
-            "4" or "four" => "4",
-            "5" or "five" => "5",
-            "6" or "six" => "6",
-            "7" or "seven" => "7",
-            "8" or "eight" => "8",
-            "9" or "nine" => "9",
-            _ => throw new InvalidOperationException($"Invalid number {number}")
+            "one" => "1",
+            "two" => "2",
+            "three" => "3",
+            "four" => "4",
+            "five" => "5",
+            "six" => "6",
+            "seven" => "7",
+            "eight" => "8",
+            "nine" => "9",
+            _ => number
         };
     }
 
-    // First group has to be a lookahead because input might have only one number so it should not be consumed
-    [GeneratedRegex("(?=(?'first'[0-9]|zero|one|two|three|four|five|six|seven|eight|nine)).*(?'last'[0-9]|zero|one|two|three|four|five|six|seven|eight|nine)")]
-    private static partial Regex FindFirstAndLastNumberFromZeroToNineIncludingWords();
+    private class Entry
+    {
+        public string FirstNumber { get; set; } = string.Empty;
+        public string LastNumber { get; set; } = string.Empty;
+        public int CombinedNumber => int.Parse(FirstNumber + LastNumber);
+    }
+
+    private static List<Entry> Deserialize(string input, Regex regex)
+    {
+        return regex
+            .Matches(input)
+            .Select(match => new Entry
+            {
+                FirstNumber = match.Groups["first"].Value,
+                LastNumber = match.Groups["last"].Value
+            })
+            .ToList();
+    }
 }
